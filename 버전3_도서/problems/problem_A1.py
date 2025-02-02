75dc6e3d644a1320028ce74fc727e652fe8c00e0
Yoo Tae Young <sps07040@gmail.com>
2025-02-02 22:07:23 +0900

    chrolling Aladin API

--- 버전3_도서/problems/problem_A1.py
+++ 버전3_도서/problems/problem_A1.py
@@ -8,7 +8,7 @@ load_dotenv()  # .env 파일을 읽어 환경 변수로 설정합니다.
 # 1. [ dotenv를 활용하여 알라딘 API 키 가져오기 ]
 MY_TTBKEY = os.getenv('나의 알라딘 API 키')
 # 2. [ 공식 문서를 참고하여 알라딘 API 검색 URL 설정하기 ]
-ALADIN_SEARCH_URL = '알라딘 검색 API URL'
+ALADIN_SEARCH_URL = 'http://www.aladin.co.kr/ttb/api/ItemSearch.aspx'
 
 
 # 3. 도서 데이터를 가져오는 함수 정의
@@ -21,10 +21,10 @@ def fetch_books(keyword):  # keyword: 검색할 키워드
         'Version': '20131101',  # API 버전
     }
     # 3.1 [ requests 문서를 참고하여 HTTP GET 요청 보내는 코드 작성하기 ]
-    pass
+    request_doc = requests.get(url=url, params=params)
 
     # 3.2 [ requests 문서를 참고하여 응답 데이터를  python의 dict 타입으로 변환하여 data 변수에 저장 ]
-    pass
+    data = request_doc.json()
 
     return data
 
@@ -37,4 +37,5 @@ books = result.get('item', [])
 # 4. [ 각 도서를 순회하며 넘버링과 제목을 출력하기 ]
 # ex) 1. 펭귄이 살아갑니다.
 #     2. 펭귄의 ...
-pass
+for idx, book in enumerate(books):
+    print(f'{idx+1}. {book['title']}')
